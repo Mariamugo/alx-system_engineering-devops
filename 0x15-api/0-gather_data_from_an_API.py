2ed9a0da9b57efba71bd57cef48f0f5704a874e3
Mariamugo <shee.mw70@gmail.com>
2024-09-19 11:47:54 -0700

    RESTful API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,14 +12,14 @@ url = "https://jsonplaceholder.typicode.com/"
 
 def employee_data(employee_id):
     user_info = requests.get(url + "users/{}".format(employee_id)).json()
-    todo_list = requests.get(url + "todos", params={"user_id": employee_id}).json()
+    todo = requests.get(url + "todos", params={"user_id": employee_id}).json()
 
     """variables to lists completed and total tasks"""
-    completed = [task.get("title") for task in todo_list if task.get("completed")]
-    total_todos = len(todo_list)
-    completed_todo_number = len(completed)
+    comp = [task.get("title") for task in todo if task.get("comp")]
+    total_todo = len(todo)
+    comp_count = len(comp)
 
-    return user_info.get("name"), completed_todo_number, total_todos, completed
+    return user_info.get("name"), comp_count, total_todo, comp
 
 
 """check whether script is imported or run directly"""
@@ -33,9 +33,9 @@ if __name__ == "__main__":
 
     """script to accept integer param"""
     employee_id = int(sys.argv[1])
-    employee_name, completed_todo_number, total_todos, completed = employee_data(employee_id)
+    employee_name, comp_count, total_todo, comp = employee_data(employee_id)
 
 
-    print("Employee {} is done with tasks {}/{}:".format(employee_name, completed_todo_number, total_todos))
-    for task in  completed:
+    print("Employee {} is done with tasks {}/{}:".format(employee_name, comp_count, total_todo))
+    for task in  comp:
         print("\t", task)
