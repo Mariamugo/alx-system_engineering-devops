02613090c8d1253912032212ada8742dcfcec817
Mariamugo <shee.mw70@gmail.com>
2024-09-19 12:05:20 -0700

    RESTful API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,8 +5,9 @@ endpoint and displays the employee name, the number of tasks completed
 out the total tasks to be completed
 """
 
-import requests
 import sys
+import requests
+
 
 def employee_data(employee_id):
     url = "https://jsonplaceholder.typicode.com/"
@@ -35,6 +36,6 @@ if __name__ == "__main__":
     employee_name, comp_count, total_todo, comp = employee_data(employee_id)
 
     print("Employee {} is done with tasks {}/{}:".format
-            (employee_name, comp_count, total_todo))
+     (employee_name, comp_count, total_todo))
     for task in comp:
         print("\t{}".format(task))
