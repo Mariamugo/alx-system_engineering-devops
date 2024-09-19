99f9b801596e3a6f66b743e65162276a687b39f6
Mariamugo <shee.mw70@gmail.com>
2024-09-19 12:58:16 -0700

    RESTful API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -36,6 +36,7 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     employee_name, comp_count, total_todo, completed = employee_data(employee_id)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, comp_count, total_todo))
+    print("Employee {} is done with tasks({}/{}):"
+            .format(employee_name, comp_count, total_todo))
     for task in completed:
         print("\t {}".format(task))
