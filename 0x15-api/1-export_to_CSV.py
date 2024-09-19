4ee7aaefdffc65835bf297e41895dfff035e9f93
Mariamugo <shee.mw70@gmail.com>
2024-09-19 14:15:19 -0700

    export_csv_format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This scripts converts a script that displays employee
+todo list (by their name and task title)
+into csv format
+"""
+
+import csv
+import requests
+import sys
+
+
+
